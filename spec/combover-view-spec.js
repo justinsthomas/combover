6dc8f28d6f7b78e8b030144574754196cce1db8e
Justin Thomas <justin.stephen.thomas@gmail.com>
2017-04-10 14:19:06 -0400

    add functionality to comb current file

---
+++ spec/combover-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ComboverView from '../lib/combover-view';
+
+describe('ComboverView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
